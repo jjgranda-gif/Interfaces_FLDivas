0e5a71975c2e1bc1897b673d29d63ad60f4505f2
Jossi <jjgranda@sudamericano.edu.ec>
2026-01-14 18:22:20 -0500

    Pequeños ajustes en el proyecto

--- src/app/page/inicio/inicio.page.ts
+++ src/app/page/inicio/inicio.page.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, IonicModule],
+  imports: [
+    IonicModule,
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage {
 
-  constructor(private router: Router) {}
+  categorias = [
+    {
+      nombre: 'Vestidos',
+      imagen: 'https://images.unsplash.com/photo-1520975916090-3105956dac38'
+    },
+    {
+      nombre: 'Blusas',
+      imagen: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab'
+    },
+    {
+      nombre: 'Accesorios',
+      imagen: 'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c'
+    }
+  ];
 
-  irStore() {
-    this.router.navigate(['/store']);
-  }
 }
